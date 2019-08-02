78fe9ccb6c0ae643b6a13d8da3ffa62114e0cbae
sasonhe <642139009@qq.com>
2019-08-02 13:48:30 +0800

    debd

--- src/components/FenDate.vue
+++ src/components/FenDate.vue
@@ -1,41 +1,57 @@
 <template>
 <div class="container maxH" style="padding:0;">
-  <van-divider :style="{ borderColor: '#263e64'}" class="bTitle">峰会议程</van-divider>
-  <van-tabs class="tab" v-model="active" animated swipeable :offset-top="76" :border="false" background="#007cc2" color="#fff" title-inactive-color="#BBDBF3" title-active-color="#fff" line-width="0">
-    <van-tab  v-for="(name,n) in fenData" :key="n" :title="name.name">
-      <div style="padding:.4rem;" class="scrollH">
-        <div class="flex fenTitle" v-for="(title,nd) in name.title" :key="nd">
-          <div class="item-flex-left timeTitle">{{title | left(title)}}</div>
-          <div class="item-flex-right timeTitle">{{title | right(title)}}</div>
-        </div>
-        <van-steps v-if="name.type == 666" direction="vertical" :active="null">
-          <van-step v-for="(item,index) in name.main" :key="index">
-            <div class="flex-wrapper">
-              <div class="flex-left">
-                <h3 class="zTitle">{{item.time}}</h3>
+  <van-divider :style="{ borderColor: '#263e64',margin:'0 .2rem'}" class="bTitle">峰会议程</van-divider>
+  <van-tabs class="tab" v-model="activeDate" animated swipeable :border="false"  background="none" color="#fff" title-inactive-color="#c0c0c0" title-active-color="#efefef" line-width="40%">
+    <van-tab  title="9月11">
+      <div class="eat" style="padding-top:.3rem;">
+        <p>下午 3:30-6:00 闭门会</p>
+        <p>粤港澳大湾区国际生物医药产业专家、政府领导闭门会议</p>
+        <p>下午 6:00-7:30 欢迎晚宴</p>
+      </div>
+    </van-tab>
+    <van-tab  title="9月12">
+      <div style="padding-top:.3rem;">
+        <van-tabs class="tab child" v-model="active" animated swipeable :offset-top="76" :border="false" background="#007cc2" color="#fff" title-inactive-color="#BBDBF3" title-active-color="#fff" line-width="0">
+          <van-tab  v-for="(name,n) in fenData" :key="n" :title="name.name">
+            <div style="padding:0.2rem;" class="scrollH">
+              <div class="flex fenTitle" v-for="(title,nd) in name.title" :key="nd">
+                <div class="item-flex-left timeTitle">{{title | left(title)}}</div>
+                <div class="item-flex-right timeTitle">{{title | right(title)}}</div>
               </div>
-              <div class="flex-right">
-                <p class="nameTitle color-B" v-for="(items,i) in item.content" :key="i+items">{{items}}</p>
-                <p class="nameTitle  color-B" v-for="(gust,ides) in item.gust" :key="ides">{{gust}}</p>
-              </div>
+              <van-steps v-if="name.type == 666" direction="vertical" :active="null">
+                <van-step v-for="(item,index) in name.main" :key="index">
+                  <div class="flex-wrapper">
+                    <div class="flex-left">
+                      <h3 class="zTitle">{{item.time}}</h3>
+                    </div>
+                    <div class="flex-right">
+                      <p class="nameTitle color-B" v-for="(items,i) in item.content" :key="i+items">{{items}}</p>
+                      <p class="nameTitle  color-B" v-for="(gust,ides) in item.gust" :key="ides">{{gust}}</p>
+                    </div>
+                  </div>
+                </van-step>
+              </van-steps>
+              <van-steps v-else direction="vertical" :active="null">
+                <van-step v-for="(item,index) in name.main" :key="index">
+                  <div class="flex-wrapper">
+                    <div class="flex-left">
+                      <h3 class="zTitle">{{item.time}}</h3>
+                    </div>
+                    <div class="flex-right">
+                      <p class="nameTitle color-B" v-for="(items,i) in item.dev" :key="i">{{items}}</p>
+                    </div>
+                  </div>
+                </van-step>
+              </van-steps>
             </div>
-          </van-step>
-        </van-steps>
-        <van-steps v-else direction="vertical" :active="null">
-          <van-step v-for="(item,index) in name.main" :key="index">
-            <div class="flex-wrapper">
-              <div class="flex-left">
-                <h3 class="zTitle">{{item.time}}</h3>
-              </div>
-              <div class="flex-right">
-                <p class="nameTitle color-B" v-for="(items,i) in item.dev" :key="i">{{items}}</p>
-              </div>
-            </div>
-          </van-step>
-        </van-steps>
+          </van-tab>
+        </van-tabs>
       </div>
     </van-tab>
   </van-tabs>
+
+
+
 
 </div>
 </template>
@@ -45,6 +61,7 @@ export default {
   data() {
     return {
       active:0,
+      activeDate:0,
       fenData:[
         {
           id:0,
@@ -55,7 +72,7 @@ export default {
             {
               id:1,
               time:'09:00-09:30',
-              content:["嘉宾签到","播放暖场宣传片"],
+              content:["嘉宾签到"],
               gust:[]
             },
             {
@@ -67,7 +84,7 @@ export default {
             {
               id:3,
               time:'09:55-10:10',
-              content:["中科院上海药研所坪山安评中心项目签约仪式"],
+              content:["项目集中签约仪式"],
               gust:[]
             },
             {
@@ -79,7 +96,7 @@ export default {
             {
               id:5,
               time:'10:25-10:35',
-              content:["主旨演讲（二）","主题待定"],
+              content:["主旨演讲（二）"],
               gust:[]
             },
             {
@@ -97,7 +114,7 @@ export default {
             {
               id:8,
               time:'11:35-12:35',
-              content:["主题讨论（一）","粤港澳生物医药产业创新合作区如何创新驱动发展（政策、人才、临床、标准等）"],
+              content:["主题讨论（一）","粤港澳生物医药产业创新合作区如何创新驱动发展"],
               gust:[]
             },
             {
@@ -109,7 +126,7 @@ export default {
             {
               id:10,
               time:'14:30-16:00',
-              content:["主题讨论（二）","1、中国生物科技公司走向全球化","2、粤港澳大湾区与创新驱动下的成果转化"],
+              content:["主题讨论（二）","中国生物科技公司走向全球化","粤港澳大湾区与创新驱动下的成果转化"],
               gust:[]
             },
             {
@@ -121,99 +138,8 @@ export default {
           ]
         },
         {
-          id:1,
-          name:"分会场 ( 一 )",
-          title:["主题：“生物医药/医疗器械”项目路演","领域：智慧医疗、精准医学、生物制药","承办单位：投壶网、E99联盟","规模：约100人"],
-          main:[
-            {
-              time:"14:00-14:10",
-              dev:["主办方致欢迎辞"]
-            },
-            {
-              time:"14:10-14:30",
-              dev:["1、主题分享：","全球科技创新下的中国生物医药产业","2、拟邀嘉宾：","仙瞳资本董事长 刘牧龙","倚锋资本创始人 朱晋桥","融昱资本业务合伙人 张颢腾"]
-            },
-            {
-              time:"14:30-15:30",
-              dev:["1、项目路演：","生物制药及智慧医疗项目路演（4-6个项目）","2、拟邀请项目：","一、天辰医疗","二、恩多克医疗","三、贝美药业","四、天蓝生物"]
-            },
-            {
-              time:"15:30-15:40",
-              dev:["项目点评"]
-            },
-            {
-              time:"15:40-16:00",
-              dev:["1、主题分享：","精准医学与生物技术创新","2、拟邀嘉宾：","深圳瑞享源基金创始合伙人 肖卫民","深圳市因诺转化医学研究院执行院长 王明军"]
-            },
-            {
-              time:"16:00-17:00",
-              dev:["1、项目路演：","国际精准医学项目路演（4个项目）","2、拟邀请项目：","五、力泰克科技","六、睿瀚医疗","七、深伦生物","八、臻质医疗"]
-            },
-            {
-              time:"17:00-17:10",
-              dev:["项目点评","拟邀嘉宾：","瑞享源基金创始合伙人肖卫民","青橙资本合伙人 张云鹏","融昱资本业务合伙人张颢腾","倍数资本创始合伙人 王阳","国科嘉和投资总监 钟靖","仙瞳资本投资总监 孙洪源","倚锋资本董事总经理 雒法涌","澳银资本投资总监 张亦笑"]
-            }
-          ]
-        },
-        {
-          id:2,
-          name:"分会场 ( 二 )",
-          title:["主题：“妇幼医疗与生命健康","领域：医学影像、体外诊断、健康管理","承办单位：邦勤资本","规模：约200人"],
-          main:[
-            {
-              time:"13:00-13:30",
-              dev:["签到、入场"]
-            },
-            {
-              time:"13:30-13:40",
-              dev:["主办方致欢迎辞，并介绍嘉宾"]
-            },
-            {
-              time:"13:40-14:10",
-              dev:["中国工程院院士 蒋庄德 教授"]
-            },
-            {
-              time:"14:10-14:15",
-              dev:["慈铭健康集团（上市公司） 韩小红博士"]
-            },
-            {
-              time:"14:15-14:35",
-              dev:["中国食品药品检定研究院主任 任海萍博士"]
-            },
-            {
-              time:"14:35-15:15",
-              dev:["妇幼生殖专家 （2名）"]
-            },
-            {
-              time:"15:15-15:35",
-              dev:["西安天隆科技有限公司 董事长 彭年才博士"]
-            },
-            {
-              time:"15:35-15:55",
-              dev:["深圳理邦智慧健康发展有限公司总经理 孔令锋 先生"]
-            },
-            {
-              time:"15:55-16:15",
-              dev:["茶歇/自由交流"]
-            },
-            {
-              time:"16:15-17:15",
-              dev:["项目路演  8分钟/家","赛雷纳（中国）医疗科技有限公司 张海川博士","深圳达盟生物科技有限公司 张崧先生","力盟生命科技(深圳)有限公司 刘湘娟女士","深圳恩多克医疗有限公司 牛会甫先生","深圳市嘉骏实业有限公司 蔡春辉先生","深圳市辰纳生物科技有限公司 于琴女士","深圳市有玺医疗科技有限公司 张楠女士"]
-            },
-            {
-              time:"17:15-17:35",
-              dev:["项目点评"]
-            },
-            {
-              time:"17:35-17:45",
-              dev:["主持人总结，结束"]
-            }
-
-          ]
-        },
-        {
           id:3,
-          name:"分会场 ( 三 )",
+          name:"分会场 ( 一 )",
           title:["主题：2019生物医药产业研发转化湾区研讨会","领域：生物医药","承办单位：深圳市生物医药促进会、广东医谷、坪山生物医药研发转化中心","规模：约100人"],
           main:[
             {
@@ -222,7 +148,7 @@ export default {
             },
             {
               time:"14:10-14:40",
-              dev:["政府领导发言（2位）"]
+              dev:["领导发言"]
             },
             {
               time:"14:40-14:55",
@@ -274,6 +200,93 @@ export default {
             }
           ]
         },
+        {
+          id:2,
+          name:"分会场 ( 二 )",
+          title:["主题：妇幼医疗与生命健康产业论坛","领域：医学影像、体外诊断、健康管理","承办单位：邦勤资本","规模：约200人"],
+          main:[
+            {
+              time:"13:30-13:40",
+              dev:["主办方致欢迎辞"]
+            },
+            {
+              time:"13:40-14:10",
+              dev:["中国工程院院士 蒋庄德 教授"]
+            },
+            {
+              time:"14:10-14:15",
+              dev:["慈铭健康集团"]
+            },
+            {
+              time:"14:15-14:35",
+              dev:["中国食品药品检定研究院"]
+            },
+            {
+              time:"14:35-15:15",
+              dev:["妇幼生殖专家"]
+            },
+            {
+              time:"15:15-15:35",
+              dev:["天隆科技"]
+            },
+            {
+              time:"15:35-15:55",
+              dev:["理邦智慧健康"]
+            },
+            {
+              time:"15:55-16:15",
+              dev:["茶歇/自由交流"]
+            },
+            {
+              time:"16:15-17:15",
+              dev:["项目路演","赛雷纳（中国）医疗、达盟生物科技、力盟生命科技、恩多克医疗、嘉骏实业、辰纳生物科技、有玺医疗科技"]
+            },
+            {
+              time:"17:15-17:35",
+              dev:["项目点评"]
+            },
+            {
+              time:"17:35-17:45",
+              dev:["主持人总结，结束"]
+            }
+
+          ]
+        },
+        {
+          id:1,
+          name:"分会场 ( 三 )",
+          title:["主题：“生物医药/医疗器械”项目路演","领域：智慧医疗、精准医学、生物制药","承办单位：投壶网、E99联盟","规模：约100人"],
+          main:[
+            {
+              time:"14:00-14:10",
+              dev:["主办方致欢迎辞"]
+            },
+            {
+              time:"14:10-14:30",
+              dev:["主题分享","全球科技创新下的中国生物医药产业","拟邀嘉宾","仙瞳资本、倚锋资本、融昱资本"]
+            },
+            {
+              time:"14:30-15:30",
+              dev:["项目路演","生物制药及智慧医疗项目","拟邀请项目","天辰医疗、恩多克医疗、贝美药业、天蓝生物等"]
+            },
+            {
+              time:"15:30-15:40",
+              dev:["项目点评"]
+            },
+            {
+              time:"15:40-16:00",
+              dev:["主题分享","精准医学与生物技术创新","拟邀嘉宾","瑞享源基金、因诺转化医学研究院"]
+            },
+            {
+              time:"16:00-17:00",
+              dev:["项目路演","国际精准医学项目","拟邀请项目","力泰克科技，睿瀚医疗，深伦生物，臻质医疗"]
+            },
+            {
+              time:"17:00-17:10",
+              dev:["项目点评","拟邀嘉宾","瑞享源基金、青橙资本、融昱资本、倍数资本、国科嘉和、仙瞳资本、倚锋资本、澳银资本"]
+            }
+          ]
+        }
       ]
     }
   },
@@ -293,19 +306,26 @@ export default {
 </script>
 
 <style>
-.maxH .tab .van-tab{
+.eat {
+  padding: .4rem .2rem;
+}
+.eat p{
+  font-size: .46rem;
+    color: #fff;
+    padding-bottom: .3rem;
+}
+.maxH .tab.child .van-tab{
   border: 1px solid #fff;
   border-left: none;
 }
-.maxH .tab .van-tab:first-child{
+.maxH .tab.child .van-tab:first-child{
   border-left: 1px solid #fff;
 }
 .flex-wrapper{
   display: flex;
 }
 .flex-left{
-  padding-left: .2rem;
-  flex: 0 0 3rem;
+  flex: 0 0 2.8rem;
 }
 .flex-right{
   flex: 1;
@@ -335,7 +355,7 @@ export default {
   line-height: .88rem;
 }
 .maxH .van-tabs__content{
-  background: #0a408a;
+  background: #0154a4;
 }
 .fenTitle .timeTitle{
   font-size: .4rem;
